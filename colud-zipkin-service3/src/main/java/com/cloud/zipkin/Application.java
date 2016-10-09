b074456acb0560811904b853823e06e4a2cc5d7a
wenxiaoqian <404190475@qq.com>
2016-10-09 15:42:38 +0800

    添加Docker demo

--- colud-zipkin-service3/src/main/java/com/cloud/zipkin/Application.java
+++ colud-zipkin-service3/src/main/java/com/cloud/zipkin/Application.java
@@ -2,12 +2,14 @@ package com.cloud.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  *
  * Created by liaokailin on 16/7/27.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
 
